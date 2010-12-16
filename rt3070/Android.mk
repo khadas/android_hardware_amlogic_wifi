852c82c5fa23493e5e0072ddb194c21c5cf4c784
Li Geng <lig@lig-Ubuntu910.(none)>
2010-12-16 16:00:49 +0800

    Modify wpa_supplicant.conf to /system/etc/wifi/

--- rt3070/Android.mk
+++ rt3070/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH:=$(call my-dir)
 
 ifeq ($(strip $(WIFI_DRIVER_MODULE_RT3070)),true)
     PRODUCT_COPY_FILES += $(LOCAL_PATH)/rt3070sta.ko:system/lib/rt3070sta.ko
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/wpa_supplicant.conf:root/data/misc/wifi/wpa_supplicant.conf
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
     PRODUCT_COPY_FILES += $(LOCAL_PATH)/RT2870STA.dat:system/etc/Wireless/RT2870STA/RT2870STA.dat
 endif
--- rtl8192cu/Android.mk
+++ rtl8192cu/Android.mk
@@ -1,4 +1,4 @@
 LOCAL_PATH:=$(call my-dir)
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/8192cu.ko:system/lib/8192cu.ko
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/wpa_supplicant.conf:root/data/misc/wifi/wpa_supplicant.conf
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
