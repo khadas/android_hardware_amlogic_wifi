3f3eb42e7090726bb7d9ecfb4939d5d062d81504
michael.zhang <michael.zhang@amlogic.com>
2011-03-31 15:10:13 +0800

    Update Ti-wgt7310 driver

--- wgt7310/Android.mk
+++ wgt7310/Android.mk
@@ -6,5 +6,7 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/wpa_supplicant.conf:system/etc/wifi/wpa_supp
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/firmware.bin:system/etc/wifi/firmware.bin
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/tiwlan_loader:system/etc/wifi/tiwlan_loader
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/tiwlan.ini:system/etc/wifi/tiwlan.ini
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/tiwlan_cu:system/etc/wifi/tiwlan_cu
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/wlan_cu:system/etc/wifi/wlan_cu
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/wlan_cu_cmd:system/etc/wifi/wlan_cu_cmd
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/tiwlan_cali.sh:system/etc/wifi/tiwlan_cali.sh
 endif
