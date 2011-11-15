c6e7367f839b589629b87881b57dcc3978cc668b
Lawrence Mok <lawrence.mok@amlogic.com>
2011-11-15 15:59:17 -0800

    change from using ALL_PREBUILT to packages

--- dhd/Android.mk
+++ dhd/Android.mk
@@ -1,30 +1,46 @@
 LOCAL_PATH:=$(call my-dir)
 
 ifeq ($(strip $(WIFI_DRIVER)),dhd)
-file := $(TARGET_OUT)/etc/wifi/wpa_supplicant.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/wpa_supplicant.conf | $(ACP)
-	$(transform-prebuilt-to-target)
+include $(CLEAR_VARS)
+LOCAL_MODULE := wpa_supplicant.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
-file := $(TARGET_OUT)/etc/nvram.txt
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/nvram.txt | $(ACP)
-	$(transform-prebuilt-to-target)
+include $(CLEAR_VARS)
+LOCAL_MODULE := nvram.txt
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
-file := $(TARGET_OUT)/etc/sdio-g-cdc-full11n-reclaim-roml-wme-idsup.bin
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/sdio-g-cdc-full11n-reclaim-roml-wme-idsup.bin | $(ACP)
-	$(transform-prebuilt-to-target)
+include $(CLEAR_VARS)
+LOCAL_MODULE := sdio-g-cdc-full11n-reclaim-roml-wme-idsup.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
-file := $(TARGET_OUT)/etc/sdio-g-cdc-roml-reclaim-wme-apsta-idsup-idauth.bin
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/sdio-g-cdc-roml-reclaim-wme-apsta-idsup-idauth.bin | $(ACP)
-	$(transform-prebuilt-to-target)
+include $(CLEAR_VARS)
+LOCAL_MODULE := sdio-g-cdc-roml-reclaim-wme-apsta-idsup-idauth.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
 endif
 
 ifeq ($(strip $(WIFI_DRIVER)),dhd_internal)
-file := $(TARGET_OUT)/etc/wifi/wpa_supplicant.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/wpa_supplicant.conf | $(ACP)
-	$(transform-prebuilt-to-target)
+include $(CLEAR_VARS)
+LOCAL_MODULE := wpa_suplicant.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 endif
