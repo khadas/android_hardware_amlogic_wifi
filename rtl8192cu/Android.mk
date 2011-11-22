76c0e54f2db01f50be299c7cb4a5cd8457c0cc3a
Jerry Cao <jerry.cao@amlogic.com>
2011-11-22 15:12:16 -0800

    Change from using ALL_PREBUILT to packages for RTL8172u

--- rtl8192cu/Android.mk
+++ rtl8192cu/Android.mk
@@ -1,13 +1,13 @@
 LOCAL_PATH:=$(call my-dir)
 
 ifeq ($(strip $(WIFI_DRIVER)),rtl8192cu)
-file := $(TARGET_OUT)/lib/8192cu.ko
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/8192cu.ko | $(ACP)
-	$(transform-prebuilt-to-target)
 
-file := $(TARGET_OUT)/etc/wifi/wpa_supplicant.conf
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/wpa_supplicant.conf | $(ACP)
-	$(transform-prebuilt-to-target)
+include $(CLEAR_VARS)
+LOCAL_MODULE := wpa_supplicant.conf
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
 endif
