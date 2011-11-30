8b989be658588aa054d5aae929f34892fe303078
eric <eric.zhong@amlogic.com>
2011-11-30 19:34:03 +0800

    wpa supplicant 8 for michael zhang

--- rtl8192cu/Android.mk
+++ rtl8192cu/Android.mk
@@ -1,13 +1,3 @@
-LOCAL_PATH:=$(call my-dir)
-
-ifeq ($(strip $(WIFI_DRIVER)),rtl8192cu)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wpa_supplicant.conf
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
+ifeq ($(WIFI_DRIVER),rtl8192cu)
+    include $(call all-subdir-makefiles)
 endif
