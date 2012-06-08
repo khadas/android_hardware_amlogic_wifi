cbe55a0f835a0c5ecc90e45c13e556b7e9ddfece
Mengchun Li <mengchun.li@amlogic.com>
2012-06-08 14:38:48 +0800

    Add Ralink 3070 support

--- rt3070/Android.mk
+++ rt3070/Android.mk
@@ -1,15 +1,3 @@
 ifeq ($(WIFI_DRIVER),rt3070)
-
-LOCAL_PATH:=$(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wpa_supplicant.conf
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/wifi
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(call all-subdir-makefiles)
-
+    include $(call all-subdir-makefiles)
 endif
