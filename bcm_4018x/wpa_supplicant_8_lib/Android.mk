1d986d009094c5b4c8ed7283e0ec43360505be1a
Lawrence Mok <lawrence.mok@amlogic.com>
2012-10-17 19:27:06 -0700

    update bcm4018x supplicant lib from Peter and BRCM

--- bcm_4018x/wpa_supplicant_8_lib/Android.mk
+++ bcm_4018x/wpa_supplicant_8_lib/Android.mk
@@ -53,14 +53,7 @@ endif
 ########################
 
 include $(CLEAR_VARS)
-
-ifeq ($(BOARD_WPA_SUPPLICANT_DRIVER),WEXT)
-LOCAL_MODULE := lib_driver_cmd_wext
-endif
-ifeq ($(BOARD_WPA_SUPPLICANT_DRIVER),NL80211)
 LOCAL_MODULE := lib_driver_cmd_nl80211
-endif
-
 LOCAL_SHARED_LIBRARIES := libc libcutils
 LOCAL_CFLAGS := $(L_CFLAGS)
 LOCAL_SRC_FILES := $(WPA_SRC_FILE)
