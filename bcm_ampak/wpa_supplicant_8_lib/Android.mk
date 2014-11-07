bfdf699b20ea5eebf916ed07e81b6423d09090dd
Lawrence Mok <lawrence.mok@amlogic.com>
2014-11-07 15:00:27 -0800

    rename supplicant library to a unique name
    lib_driver_cmd_bcmdhd to lib_driver_cmd_bcmdhd{_ampak,_usi}
    
    Change-Id: I0718cdea9d3b544bcfb8b3c73df385baa8266188

--- bcm_ampak/wpa_supplicant_8_lib/Android.mk
+++ bcm_ampak/wpa_supplicant_8_lib/Android.mk
@@ -57,7 +57,7 @@ endif
 ########################
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := lib_driver_cmd_bcmdhd
+LOCAL_MODULE := lib_driver_cmd_bcmdhd_ampak
 LOCAL_SHARED_LIBRARIES := libc libcutils
 LOCAL_CFLAGS := $(L_CFLAGS)
 LOCAL_SRC_FILES := $(WPA_SRC_FILE)
