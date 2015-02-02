6d872d62733f104233963afb403d953dfe769882
Gangfeng Xu <gangfeng.xu@amlogic.com>
2015-02-02 14:51:41 +0800

    PD#103095:fix compile error
    Change-Id: Ic436f8ba4f418841de53be2294db0a5c66abe4ce

--- rtl8189es/wpa_supplicant_8_lib/driver_cmd_nl80211.c
+++ rtl8189es/wpa_supplicant_8_lib/driver_cmd_nl80211.c
@@ -59,10 +59,15 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 			ret = os_snprintf(buf, buf_len,
 					  "Macaddr = " MACSTR "\n", MAC2STR(macaddr));
 	} else { /* Use private command */
+		if (os_strcasecmp(cmd, "BTCOEXSCAN-STOP") == 0 ||
+		        os_strcasecmp(cmd, "RXFILTER-STOP") == 0 ||
+		        os_strncasecmp(cmd, "SETBAND", 7) == 0) {
+			return -1;
+		}
 		os_memcpy(buf, cmd, strlen(cmd) + 1);
 		memset(&ifr, 0, sizeof(ifr));
 		memset(&priv_cmd, 0, sizeof(priv_cmd));
-		os_strncpy(ifr.ifr_name, bss->ifname, IFNAMSIZ);
+		strncpy(ifr.ifr_name, bss->ifname, IFNAMSIZ);
 
 		priv_cmd.buf = buf;
 		priv_cmd.used_len = buf_len;
