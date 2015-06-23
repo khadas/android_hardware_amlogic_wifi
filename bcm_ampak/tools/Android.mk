9e8c9d9ed4fed5936c0253ea6f247676024a093a
Rongjun Chen <rongjun.chen@amlogic.com>
2015-06-23 19:16:46 +0800

    add bcm43458 bt support
    Change-Id: Ib3f150c68a64dc7d046e7fc08aec0b2152e3f5f3

--- bcm_ampak/tools/Android.mk
+++ bcm_ampak/tools/Android.mk
@@ -1,6 +1,5 @@
 LOCAL_PATH:=$(call my-dir)
 
-ifneq ($(strip $(filter bcm4018%, $(WIFI_DRIVER))),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := wl
@@ -18,24 +17,3 @@ LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
-endif
-
-ifneq ($(strip $(filter AP6%, $(WIFI_DRIVER))),)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wl
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := dhd
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-endif
