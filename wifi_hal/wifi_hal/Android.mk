ca68d24e400335136d81da487da0c0f145b5053c
Baocheng Sun <baocheng.sun@amlogic.com>
2018-08-10 11:22:52 +0800

    wifi: modify for vndk enable
    PD# 171375
    
    modify for vndk enable
    
    Change-Id: I320435b50e5f66b2175ccb7a0793f34995405a75

--- wifi_hal/wifi_hal/Android.mk
+++ wifi_hal/wifi_hal/Android.mk
@@ -23,6 +23,8 @@ LOCAL_CFLAGS := -Wno-unused-parameter
 LOCAL_C_INCLUDES += \
 	external/libnl/include \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
+	system/core/libutils/include \
+	system/core/liblog/include \
 	external/wpa_supplicant_8/src/drivers
 
 LOCAL_SRC_FILES := \
